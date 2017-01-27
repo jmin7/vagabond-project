0a3f39cbe6b155a1b92342b7a30033d36147fb1b
Jacob Nunn <jacobnunn92@gmail.com>
2017-01-27 16:02:59 -0500

    CRUD is fine tuned for cities and posts

--- vagabond/vagabond/app/controllers/comments_controller.rb
+++ vagabond/vagabond/app/controllers/comments_controller.rb
@@ -1,2 +1,30 @@
 class CommentsController < ApplicationController
+    def new
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.new
+        puts '@post here is #{@post.id}'
+
+    end
+            
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(params[:body])
+        
+        if @comment.save 
+        redirect_to '/'
+        end
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
