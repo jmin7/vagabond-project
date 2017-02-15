7c5b2bbdb97e453f888b0b9957f1e99225f925af
Jungmin <jungmin.yjm@gmail.com>
2017-02-14 21:40:26 -0500

    styling

--- vagabond/vagabond/db/seeds.rb
+++ vagabond/vagabond/db/seeds.rb
@@ -10,9 +10,6 @@ City.destroy_all
 Post.destroy_all
 
 City.create(city_name: "Atlanta", photo_url: "https://s-media-cache-ak0.pinimg.com/originals/35/3a/8b/353a8bee54a37d8948c37b09e1865cc2.jpg")
-City.create(city_name: "London", photo_url: "https://static01.nyt.com/images/2015/12/09/travel/09intransitphoto-london/09intransitphoto-london-facebookJumbo.jpg")
-City.create(city_name: "San Francisco", photo_url: "https://cdn3.tnwcdn.com/wp-content/blogs.dir/1/files/2015/08/SANFRANCISCO.jpg")
+City.create(city_name: "London", photo_url: "https://www.alumni.ucdavis.edu/local_resources/images/benefits/travel/2017/london-and-environs/london%202.jpg")
+City.create(city_name: "San Francisco", photo_url: "https://www.t3advisors.com/wp-content/uploads/2014/07/DowntownSF.jpg")
 City.create(city_name: "Seattle", photo_url: "http://www.bekinsmovingandstorage.com/wp-content/uploads/2016/03/SeattleCity2.jpg")
-
-
-Post.create(title: "Cool City", body:"Walk the Beltline!", city_id: 1)
